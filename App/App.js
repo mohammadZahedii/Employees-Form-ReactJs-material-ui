19e6d81420f2b8d224b34174753825cd10aa58d3
mohammadZahedii <mohammadzahedi_1995@yahoo.com>
2021-10-20 04:49:57 +0330

    Apply changes on Theme

--- App/App.js
+++ App/App.js
@@ -8,7 +8,7 @@ const theme =createTheme({
     palette:{
         primary:{
             main:'#333996',
-            light:'#77ACF1',
+            light:'#A2D2FF',
         },
         secondary:{
             main:'#f83245',
@@ -17,6 +17,30 @@ const theme =createTheme({
         background:{
             default:'#f4f5fd'
         },
+      
+    },
+    shape:{
+        borderRadius:'4px'
+    },
+  
+    components:{
+        MuiInputBase:{
+            styleOverrides:{
+                root:{
+                    color:'#333996',
+                    backgroundColor:'#E8F6EF',
+                    opacity:'0.6',
+                    padding:'0 8px',
+                    borderRadius:'4px',
+                    '&:hover':{
+                        backgroundColor:'#f2f2f2'
+                    },
+                    '& .MuiSvgIcon-root':{
+                        marginRight:'8px'
+                    }
+                }
+            }
+        }
     }
 })
 
