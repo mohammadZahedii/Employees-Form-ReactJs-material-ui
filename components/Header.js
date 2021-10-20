0b08d4c630111cb050d1985603a8959cdd638e47
mohammadZahedii <mohammadzahedi_1995@yahoo.com>
2021-10-20 04:15:37 +0330

    Customize Header.js component

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,52 @@
 import React from 'react'
-
+import { AppBar } from '@mui/material'
+import { Toolbar } from '@mui/material'
+import Grid from '@mui/material/Grid'
+import { InputBase } from '@mui/material'
+import { Badge } from '@mui/material'
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
+import { IconButton } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search';
 
 export default function Header(){
 
-    return <div>header</div>
+    return (
+        <AppBar 
+        
+        position="absolute"
+            sx={{
+                backgroundColor:t=>t.palette.primary.light,
+                width:['100%','100%','calc(100% - 320px)'],
+            }}
+        >
+            <Toolbar>
+                <Grid container alignItems="center">
+                    <Grid item sx={{flex:1}}>
+                        <InputBase
+                            placeholder="my search box"
+                            startAdornment={<SearchIcon fontSize="small"/>}
+                        />
+                    </Grid>
+                    <Grid item >
+                        <IconButton>
+                            <Badge badgeContent={5} color="primary">
+                                <NotificationsNoneIcon />
+                            </Badge>
+                        </IconButton>
+                        <IconButton>
+                            <Badge badgeContent={4} color="secondary">
+                                <ChatBubbleOutlineIcon/>
+                            </Badge>
+                        </IconButton>
+                        <IconButton>
+                            <PowerSettingsNewIcon/>
+                        </IconButton>
+                    </Grid>
+                </Grid>
+            </Toolbar>
+        </AppBar>
+    )
 
 }
